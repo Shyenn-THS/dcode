b4775acebebaf1074145486e3732168501f28f7d
m99Tanishq <21bcs164@ietdavv.edu.in>
2023-01-15 00:58:45 +0530

    INIT

--- pages/support-us.tsx
+++ pages/support-us.tsx
@@ -1,30 +1,80 @@
-import { utils } from 'ethers';
 import React, { useState } from 'react';
 import toast from 'react-hot-toast';
-import { useSendTransaction, usePrepareSendTransaction } from 'wagmi';
+import { RouterProtocol } from "@routerprotocol/router-js-sdk"
+import { ethers } from "ethers";
 
 type Props = {};
 
 const SupportUs = (props: Props) => {
-  const [value, setValue] = useState('0.1');
-  const [other, setOther] = useState(false);
 
-  const { config } = usePrepareSendTransaction({
-    request: {
-      to: '0xc8261Ba37F8170f5926d22cEd1cFcD59Cd1C351E', // My Address
-      value: parseFloat(value) ? utils.parseEther(value) : '0.1', // In Matic
-    },
-  });
+  const main = async() => {
 
-  const { data, sendTransaction } = useSendTransaction({
-    ...config,
-    onSuccess: (data) =>
-      toast.success(
-        `Donated ${value} MATIC sucessfully, Thank you for your donation.`
-      ),
-    onError: (error) => toast.error('Oops, some error occured'),
-    onSettled: (data, error) => console.log('SETTLED', data, error),
-  });
+    // initialize a RouterProtocol instance
+    let SDK_ID = '24' // get your unique sdk id by contacting us on Telegram
+    let chainId = '137'
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts', []) // connects MetaMask
+    const signer = provider.getSigner()
+    const routerprotocol = new RouterProtocol(SDK_ID, chainId, provider)
+    await routerprotocol.initialize()
+    
+    // get a quote for USDC transfer from Polygon to Fantom
+    let args = {
+        amount: (ethers.utils.parseUnits("10.0", 6)).toString(), // 10 USDC
+        dest_chain_id: '250', // Fantom
+        src_token_address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", // USDC on Polygon
+        dest_token_address: "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75", // USDC on Fantom
+        user_address: "YOUR_WALLET_ADDRESS",
+        fee_token_address: "0x16ECCfDbb4eE1A85A33f3A9B21175Cd7Ae753dB4", // ROUTE on Polygon
+        slippage_tolerance: 2.0
+    }
+    
+    const quote = await routerprotocol.getQuote(args.amount, args.dest_chain_id, args.src_token_address, args.dest_token_address, args.user_address, args.fee_token_address, args.slippage_tolerance)
+    
+    // get allowance and give the relevant approvals
+    const wallet = new ethers.Wallet("YOUR_PRIVATE_KEY", provider) // provider was set up while initializing an instance of RouterProtocol
+    
+    let src_token_allowance = await routerprotocol.getSourceTokenAllowance(args.src_token_address, args.dest_chain_id, args.user_address)
+    if(src_token_allowance.lt(ethers.constants.MaxUint256)){
+            await routerprotocol.approveSourceToken(args.src_token_address, args.user_address, ethers.constants.MaxUint256.toString(), args.dest_chain_id, wallet)
+    }
+    if(ethers.utils.getAddress(args.src_token_address) !== ethers.utils.getAddress(args.fee_token_address)){
+        let fee_token_allowance = await routerprotocol.getFeeTokenAllowance(args.fee_token_address, args.dest_chain_id, args.user_address)
+        if(fee_token_allowance.lt(ethers.constants.MaxUint256)){
+            await routerprotocol.approveFeeToken(args.fee_token_address, args.user_address, ethers.constants.MaxUint256.toString(), wallet)
+        }
+    }
+    
+    // execute the transaction
+    let tx: ethers.providers.TransactionResponse;
+    try{
+        tx = await routerprotocol.swap(quote,wallet)
+        console.log(`Transaction successfully completed. Tx hash: ${tx.hash}`)
+    }
+    catch(e){
+        console.log(`Transaction failed with error ${e}`)
+        return
+    }
+    
+    // fetching the status of the transaction
+    setTimeout(async function() {
+        let status = await routerprotocol.getTransactionStatus(tx.hash) 
+        console.log(status)
+        if (status?.tx_status_code === 1) {
+            console.log("Transaction completed")
+          // handle the case where the transaction is complete 
+        }
+        else if (status?.tx_status_code === 0) {
+            console.log("Transaction still pending")
+        // handle the case where the transaction is still pending
+        }
+      }, 180000); // waiting for sometime before fetching the status of the transaction because it may take some time for the transaction to get indexed
+    
+    }
+    
+    main()
+
+  
 
   return (
     <div className="flex justify-center items-center bg-froly-400 min-h-screen">
@@ -111,7 +161,7 @@ const SupportUs = (props: Props) => {
                 Other
               </label>
             </div>
-            {other ? (
+            { ? (
               <input
                 type="number"
                 min={0.1}
@@ -137,7 +187,7 @@ const SupportUs = (props: Props) => {
             </div> */}
             <div className="my-4 flex justify-between">
               <button
-                onClick={() => sendTransaction?.()}
+                onClick={() => main?.()}
                 className="h-10 w-32 bg-froly-500 text-sm text-white rounded-lg cursor-pointer transition-all hover:bg-rajah-200 "
               >
                 Donate Now
